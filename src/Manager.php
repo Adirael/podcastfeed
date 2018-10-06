07e3d6674638fc7f2e0710f32fe2fc065e4a7283
barredo <barredo@users.noreply.github.com>
2018-10-06 12:17:08 +0200

    Add files via upload

---
+++ src/Manager.php
@@ -0,0 +1,402 @@
+<?php
+
+namespace Torann\PodcastFeed;
+
+use DateTime;
+
+class Manager
+{
+    /**
+     * Package Config
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * General title of the podcast
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Subtitle of the podcast.
+     *
+     * @var string|null
+     */
+    private $subtitle;
+
+    /**
+     * Description of the podcast.
+     *
+     * @var string
+     */
+    private $description;
+
+    /**
+     * Summary of the podcast.
+     *
+     * @var string
+     */
+    private $summary;
+
+    /**
+     * URL to the podcast website.
+     *
+     * @var string
+     */
+    private $link;
+
+    /**
+     * URL to the image representing the podcast.
+     *
+     * @var string
+     */
+    private $image;
+
+    /**
+     * Author of the podcast.
+     *
+     * @var string
+     */
+    private $author;
+
+    /**
+     * Categories of the podcast.
+     *
+     * @var array
+     */
+    private $categories = [];
+
+    /**
+     * Explicit flag of the podcast.
+     *
+     * @var string
+     */
+    private $explicit = null;
+
+    /**
+     * Language of the podcast.
+     *
+     * @var string
+     */
+    private $language = null;
+
+    /**
+     * Date of the last publication of the podcast.
+     *
+     * @var DateTime
+     */
+    private $pubDate;
+
+    /**
+     * Email address of the owner of the podcast.
+     *
+     * @var string
+     */
+    private $email = null;
+
+    /**
+     * Copyright podcast.
+     *
+     * @var string
+     */
+    private $copyright = null;
+
+    /**
+     * Atom link of the feed.
+     *
+     * @var string
+     */
+    private $atom_link;
+
+    /**
+     * List of media for the podcast.
+     *
+     * @var array
+     */
+    private $media = [];
+
+    /**
+     * Class constructor.
+     *
+     * @param  array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+
+        // Set default headers
+        $this->setHeader([]);
+    }
+
+    /**
+     * Set the header of the podcast feed
+     *
+     * @param mixed $data
+     */
+    public function setHeader($data)
+    {
+        // Required
+        $this->title = $this->getValue($data, 'title');
+        $this->pubDate = $this->getValue($data, 'pubDate');
+        $this->feed_type = $this->getValue($data, 'feed_type');
+        $this->description = $this->getValue($data, 'description');
+        $this->summary = $this->getValue($data, 'summary');
+        $this->link = $this->getValue($data, 'link');
+        $this->image = $this->getValue($data, 'image');
+        $this->author = $this->getValue($data, 'author');
+        $this->categories = $this->getValue($data, 'categories');
+        $this->atom_link = $this->getValue($data, 'atom_link');
+        $this->main_country = $this->getValue($data, 'main_country');
+
+        // Optional values
+        $this->explicit = $this->getValue($data, 'explicit');
+        $this->subtitle = $this->getValue($data, 'subtitle');
+        $this->language = $this->getValue($data, 'language');
+        $this->email = $this->getValue($data, 'email');
+        $this->copyright = $this->getValue($data, 'copyright');
+    }
+
+    /**
+     * Get value from data and escape it.
+     *
+     * @param  mixed  $data
+     * @param  string $key
+     *
+     * @return mixed
+     */
+    public function getValue($data, $key)
+    {
+        $value = array_get($data, $key, $this->getDefault($key));
+
+        // Avoid escaping categories to confort to the itunes spec
+        if($key == 'categories') {
+          return $value;
+        }
+
+        return htmlspecialchars($value);
+    }
+
+    /**
+     * Add media to the podcast feed.
+     *
+     * @param array $media
+     */
+    public function addMedia(array $media)
+    {
+        $this->media[] = new Media($media);
+    }
+
+    /**
+     * Returns the podcast generated as character strings
+     *
+     * @return  string
+     */
+    public function toString()
+    {
+        return $this->generate()->saveXML();
+    }
+
+    /**
+     * Returns the podcast generated as DOM document
+     *
+     * @return  \DOMDocument
+     */
+    public function toDom()
+    {
+        return $this->generate();
+    }
+
+    /**
+     * Get default value from config
+     *
+     * @param  string $key
+     * @param  mixed  $fallback
+     *
+     * @return mixed
+     */
+    public function getDefault($key, $fallback = null)
+    {
+        return array_get($this->config['defaults'], $key, $fallback);
+    }
+
+    /**
+     * Generate the DOM document
+     *
+     * @return \DOMDocument
+     */
+    private function generate()
+    {
+        // Create the DOM
+        $dom = new \DOMDocument("1.0", "utf-8");
+
+        // Create the <rss>
+        $rss = $dom->createElement("rss");
+        $rss->setAttribute("xmlns:itunes", "http://www.itunes.com/dtds/podcast-1.0.dtd");
+        $rss->setAttribute("version", "2.0");
+        $rss->setAttribute("xmlns:content","http://purl.org/rss/1.0/modules/content/");
+        $rss->setAttribute("xmlns:atom", "http://www.w3.org/2005/Atom");
+        $rss->setAttribute("xmlns:spotify","http://www.spotify.com/ns/rss");
+        $dom->appendChild($rss);
+
+        // Create the <channel>
+        $channel = $dom->createElement("channel");
+        $rss->appendChild($channel);
+
+        // Add atom:link for interoperability
+        $atom = $dom->createElement("atom:link");
+        $atom->setAttribute("href", $this->atom_link);
+        $atom->setAttribute("rel", "self");
+        $atom->setAttribute("type", "application/rss+xml");
+        $channel->appendChild($atom);
+
+        // Create the <title>
+        $title = $dom->createElement("title", $this->title);
+        $channel->appendChild($title);
+
+        // Create the <itunes:subtitle>
+        if ($this->subtitle != null) {
+            $itune_subtitle = $dom->createElement("itunes:subtitle", $this->subtitle);
+            $channel->appendChild($itune_subtitle);
+        }
+
+        // Create the <itunes:type>
+        if (!empty($this->feed_type)
+            && ($this->feed_type == 'serial'
+            OR  $this->feed_type == 'episodic')) {
+            $itune_type = $dom->createElement("itunes:type", $this->feed_type);
+            $channel->appendChild($itune_type);
+        }
+
+        // Create the <link>
+        $link = $dom->createElement("link", $this->link);
+        $channel->appendChild($link);
+
+        $spotify_limit = $dom->createElement("spotify:limit");
+        $spotify_limit->setAttribute("recentCount", "10");
+        $channel->appendChild($spotify_limit);
+
+        $countries = array('ar','cl','co','es','mx','pe','us');
+        foreach($countries as $k => $v) {
+            if($v == strtolower($this->main_country)) {
+                unset($countries[$k]);
+            }
+        }
+        $spotify_country = $dom->createElement(
+            "spotify:countryOfOrigin",
+            strtolower($this->main_country).' '.implode(' ',$countries)
+        );
+        $channel->appendChild($spotify_country);
+
+        // Create the <description>
+        $description = $dom->createElement("description");
+        $description->appendChild($dom->createCDATASection($this->description));
+        $channel->appendChild($description);
+
+        // Create the <itunes:summary>
+        $itune_summary = $dom->createElement("itunes:summary", $this->summary);
+        $channel->appendChild($itune_summary);
+
+        // Create the <image>
+        $image = $dom->createElement("image");
+        $image->appendChild($title->cloneNode(true));
+        $image->appendChild($link->cloneNode(true));
+        $channel->appendChild($image);
+        $image_url = $dom->createElement("url", $this->image);
+        $image->appendChild($image_url);
+
+        // Create the <itunes:image>
+        $itune_image = $dom->createElement("itunes:image");
+        $itune_image->setAttribute("href", $this->image);
+        $channel->appendChild($itune_image);
+
+        // Create the <itunes:author>
+        $itune_author = $dom->createElement("itunes:author", $this->author);
+        $channel->appendChild($itune_author);
+
+        // Create the <itunes:owner>
+        $itune_owner = $dom->createElement("itunes:owner");
+        $itune_owner_name = $dom->createElement("itunes:name", $this->author);
+        $itune_owner->appendChild($itune_owner_name);
+        if ($this->email != null) {
+            $itune_owner_email = $dom->createElement("itunes:email", $this->email);
+            $itune_owner->appendChild($itune_owner_email);
+        }
+        $channel->appendChild($itune_owner);
+
+        // Create the <itunes:category>
+        foreach ($this->categories as $category => $subcategories) {
+            $node = $channel->appendChild($dom->createElement('itunes:category'));
+            $node->setAttribute("text", $category);
+
+            foreach ($subcategories as $subcategory => $subcategories) {
+                if(is_array($subcategories)) {
+                    $subnode = $node->appendChild($dom->createElement('itunes:category'));
+                    $subnode->setAttribute("text", $subcategory);
+
+                    foreach($subcategories as $subsubcategory) {
+                        $subsubnode = $subnode->appendChild($dom->createElement('itunes:category'));
+                        $subsubnode->setAttribute("text", $subsubcategory);
+                    }
+                } else {
+                    $subnode = $node->appendChild($dom->createElement('itunes:category'));
+                    $subnode->setAttribute("text", $subcategories);
+                }
+            }
+
+            $channel->appendChild($node);
+        }
+
+        // Create the <itunes:explicit>
+        if ($this->explicit !== null) {
+            $explicit = $dom->createElement("itunes:explicit", $this->explicit);
+            $channel->appendChild($explicit);
+        }
+
+        // Create the <language>
+        if ($this->language !== null) {
+            $language = $dom->createElement("language", $this->language);
+            $channel->appendChild($language);
+        }
+
+        // Create the <copyright>
+        if ($this->copyright !== null) {
+            $copyright = $dom->createElement("copyright", $this->copyright);
+            $channel->appendChild($copyright);
+        }
+
+        // Create the <pubDate>
+        if ($this->pubDate == null) {
+            $this->pubDate = new DateTime();
+        }
+        if (is_string($this->pubDate)) {
+            $this->pubDate = new DateTime($this->pubDate);
+        }
+
+        $pubDate = $dom->createElement("pubDate", $this->pubDate->format(DATE_RFC2822));
+        $channel->appendChild($pubDate);
+        $lastBuildDate = $dom->createElement("lastBuildDate", $this->pubDate->format(DATE_RFC2822));
+        $channel->appendChild($lastBuildDate);
+
+        // Create the <items>
+        foreach ($this->media as $media) {
+            // Addition of media in the dom
+            $media->addToDom($dom);
+
+            // Get the latest date media for <pubDate>
+            if ($this->pubDate == null) {
+                $this->pubDate = $media->getPubDate();
+            }
+            else {
+                if ($this->pubDate < $media->getPubDate()) {
+                    $this->pubDate = $media->getPubDate();
+                }
+            }
+        }
+
+        // Return the DOM
+        return $dom;
+    }
+}
---
+++ src/Media.php
@@ -0,0 +1,311 @@
+<?php namespace Torann\PodcastFeed;
+
+use DateTime;
+use DOMDocument;
+
+class Media
+{
+    /**
+     * Title of media.
+     *
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Subtitle of media.
+     *
+     * @var string|null
+     */
+    private $subtitle;
+
+    /**
+     * URL to the media web site.
+     *
+     * @var string
+     */
+    private $link;
+
+    /**
+     * Date of publication of the media.
+     *
+     * @var DateTime
+     */
+    private $pubDate;
+
+    /**
+     * description media.
+     *
+     * @var string
+     */
+    private $description;
+
+    /**
+     * summary media.
+     *
+     * @var string
+     */
+    private $summary;
+
+    /**
+     * URL of the media
+     *
+     * @var string
+     */
+    private $url;
+
+    /**
+     * Type of media (audio / mpeg, for example).
+     *
+     * @var string
+     */
+    private $type;
+
+    /**
+     * Author of the media.
+     *
+     * @var string
+     */
+    private $author;
+
+    /**
+     * GUID of the media.
+     *
+     * @var string
+     */
+    private $guid;
+
+    /**
+     * GUID isPermaLink attribute
+     *
+     * @var string
+     */
+    private $isPermaLink;
+
+    /**
+     * Duration of the media only as HH:MM:SS, H:MM:SS, MM:SS or M:SS.
+     *
+     * @var string
+     */
+    private $duration;
+
+    /**
+     * Explicit flag of the media.
+     *
+     * @var string
+     */
+    private $explicit;
+
+    /**
+     * URL to the image representing the media.
+     *
+     * @var string
+     */
+    private $image;
+
+    /**
+     * Length in bytes of the media file.
+     *
+     * @var string
+     */
+    private $length;
+
+    /**
+     * Class constructor
+     *
+     * @param array $data
+     */
+    public function __construct($data)
+    {
+        $this->title = $this->getValue($data, 'title');
+        $this->subtitle = $this->getValue($data, 'subtitle');
+        $this->summary = $this->getValue($data, 'summary', null, false);
+        $this->description = $this->getValue($data, 'description', null, true);
+        $this->content_encoded = $this->getValue($data, 'content_encoded', null, true);
+        $this->link = $this->getValue($data, 'link', null, false);
+        $this->pubDate = $this->getValue($data, 'publish_at');
+        $this->url = $this->getValue($data, 'url');
+        $this->guid = $this->getValue($data, 'guid');
+        $this->type = $this->getValue($data, 'type');
+        $this->duration = $this->getValue($data, 'duration');
+        $this->explicit = $this->getValue($data, 'explicit');
+        $this->author = $this->getValue($data, 'author');
+        $this->feed_season = $this->getValue($data, 'feed_season');
+        $this->feed_episode = $this->getValue($data, 'feed_episode');
+        $this->image = $this->getValue($data, 'image');
+        $this->length = $this->getValue($data, 'length');
+        $this->isPermaLink = $this->getValue($data, 'isPermaLink');
+
+        // Ensure publish date is a DateTime instance
+        if (is_string($this->pubDate)) {
+            $this->pubDate = new DateTime($this->pubDate);
+        }
+    }
+
+    /**
+     * Get value from data and escape it.
+     *
+     * @param  mixed  $data
+     * @param  string $key
+     * @param  mixed $default
+     *
+     * @return string
+     */
+    public function getValue($data, $key, $default = null, $raw = false)
+    {
+        $value = array_get($data, $key, $default);
+
+        if(!$raw) {
+            return htmlspecialchars($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Get media publication date.
+     *
+     * @return  DateTime
+     */
+    public function getPubDate()
+    {
+        return $this->pubDate;
+    }
+
+    /**
+     * Adds media in the DOM document setting.
+     *
+     * @param DOMDocument $dom
+     */
+    public function addToDom(DOMDocument $dom)
+    {
+        // Recovery of  <channel>
+        $channels = $dom->getElementsByTagName("channel");
+        $channel = $channels->item(0);
+
+        // Create the <item>
+        $item = $dom->createElement("item");
+        $channel->appendChild($item);
+
+        // Create the <title>
+        $title = $dom->createElement("title", $this->title);
+        $item->appendChild($title);
+
+        // Create the <description>
+
+        // // PARA SPOTIFY
+        // // DESCRIPTION CON HTML
+        //
+        // // $description = $dom->createElement("description");
+        // // $description->appendChild($dom->createCDATASection($this->description));
+        // // $item->appendChild($description);
+        //
+        //
+        // // DESCRIPTION CON SUMARIO
+        // $description = $dom->createElement("description", $this->summary);
+        // $item->appendChild($description);
+        //
+        // $content_encoded = $dom->createElement("content:encoded");
+        // $content_encoded->appendChild($dom->createCDATASection($this->description));
+        // $item->appendChild($content_encoded);
+
+        // PARA SPOTIFY
+        // DESCRIPTION CON HTML
+
+        // $description = $dom->createElement("description");
+        // $description->appendChild($dom->createCDATASection($this->description));
+        // $item->appendChild($description);
+
+        // TRADICIONAL
+        if(empty($this->content_encoded)) {
+          // Create the <itunes:subtitle>
+          if ($this->subtitle) {
+              $itune_subtitle = $dom->createElement("itunes:subtitle", $this->subtitle);
+              $item->appendChild($itune_subtitle);
+          }
+
+          // Create the <itunes:summary>
+          $itune_summary = $dom->createElement("itunes:summary", $this->summary);
+          $item->appendChild($itune_summary);
+
+          // $description = $dom->createElement("description");
+          // $description->appendChild($dom->createCDATASection($this->description));
+          // $item->appendChild($description);
+
+          $content_encoded = $dom->createElement("content:encoded");
+          $content_encoded->appendChild($dom->createCDATASection($this->content_encoded));
+          $item->appendChild($content_encoded);
+
+          // SPOTIFY
+        } else {
+
+          // Create the <itunes:summary>
+          $itune_summary = $dom->createElement("itunes:summary", $this->summary);
+          $item->appendChild($itune_summary);
+
+          $description = $dom->createElement("description", $this->summary);
+          $item->appendChild($description);
+
+          $content_encoded = $dom->createElement("content:encoded");
+          $content_encoded->appendChild($dom->createCDATASection($this->content_encoded));
+          $item->appendChild($content_encoded);
+        }
+
+        // Create the <pubDate>
+        $pubDate = $dom->createElement("pubDate", $this->pubDate->format(DATE_RFC2822));
+        $item->appendChild($pubDate);
+
+        // Create the <enclosure>
+        $enclosure = $dom->createElement("enclosure");
+        $enclosure->setAttribute("url", $this->url);
+        $enclosure->setAttribute("type", $this->type);
+        $enclosure->setAttribute("length", $this->length);
+        $item->appendChild($enclosure);
+
+        // Create the author
+        if ($this->author) {
+            // Create the <author>
+            $author = $dom->createElement("author", $this->author);
+            $item->appendChild($author);
+
+            // Create the <itunes:author>
+            $itune_author = $dom->createElement("itunes:author", $this->author);
+            $item->appendChild($itune_author);
+        }
+        if ($this->link) {
+            // Create the <link>
+            $link = $dom->createElement("link", $this->link);
+            $item->appendChild($link);
+        }
+
+        if ($this->feed_season > 0) {
+            $feed_season = $dom->createElement("itunes:season", intval($this->feed_season));
+            $item->appendChild($feed_season);
+        }
+
+        if ($this->feed_episode > 0) {
+            $feed_episode = $dom->createElement("itunes:episode", intval($this->feed_episode));
+            $item->appendChild($feed_episode);
+        }
+
+        // Create the <itunes:duration>
+        $itune_duration = $dom->createElement("itunes:duration", $this->duration);
+        $item->appendChild($itune_duration);
+
+        // Create the <itunes:explicit>
+        $explicit = $dom->createElement("itunes:explicit", (is_null($this->explicit) OR !$this->explicit OR empty($this->explicit) OR $this->explicit == 'no') ? 'clean' : 'yes');
+        $item->appendChild($explicit);
+
+        // Create the <guid>
+        $guid = $dom->createElement("guid", $this->guid);
+        $guid->setAttribute("isPermaLink", $this->isPermaLink);
+        $item->appendChild($guid);
+
+        // Create the <itunes:image>
+        if ($this->image) {
+            $itune_image = $dom->createElement("itunes:image");
+            $itune_image->setAttribute("href", $this->image);
+            $item->appendChild($itune_image);
+        }
+    }
+}
