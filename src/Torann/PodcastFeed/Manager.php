7634e959b0200b7514db36a620fc2e72f0a520d1
Álex Barredo <barredo@users.noreply.github.com>
2020-06-04 01:24:56 +0200

    Update Manager.php
    Quitar el de spotify que no carga y añadir nueva URL de Google

--- src/Torann/PodcastFeed/Manager.php
+++ src/Torann/PodcastFeed/Manager.php
@@ -237,9 +237,9 @@ class Manager
         $rss->setAttribute("xmlns:itunes", "http://www.itunes.com/dtds/podcast-1.0.dtd");
         $rss->setAttribute("version", "2.0");
         $rss->setAttribute("xmlns:content","http://purl.org/rss/1.0/modules/content/");
-        $rss->setAttribute("xmlns:googleplay","http://www.google.com/schemas/play-podcasts/1.0");
+        $rss->setAttribute("xmlns:googleplay","https://www.google.com/schemas/play-podcasts/1.0/play-podcasts.xsd");
         $rss->setAttribute("xmlns:atom", "http://www.w3.org/2005/Atom");
-        $rss->setAttribute("xmlns:spotify","http://www.spotify.com/ns/rss");
+        // $rss->setAttribute("xmlns:spotify","http://www.spotify.com/ns/rss");
         $dom->appendChild($rss);
 
         // Create the <channel>
